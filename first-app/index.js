94a6ffe3f82ab22248da864914cab40402adc927
Sachin7717 <sachindhimann71@gmail.com>
2024-08-12 12:49:45 +0530

    made a first.html file and it is imported in index.js

--- first-app/index.js
+++ first-app/index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-const port = 4000
+const port = 5000
 
 app.get('/', (req,res) =>{
     res.send('hello')
@@ -15,9 +15,9 @@ app.listen(process.env.PORT,() => {
     
 })
 
-app.get('/instagram',(req,res)=>{
-    res.send('this is instagram')
-})
+app.get('/instagram', (req, res) => {
+    res.sendFile(__dirname + '/first.html');
+});
 
 app.get('/github', (req,res) => {
     res.send('this is github sachin7717')
